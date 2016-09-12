5b4627c57d2c7681567f496bbbb34528a570acda
bradphelan <b.phelan@weingartner.com>
2016-09-12 17:09:44 +0200

    Add the ability to build nuget packages

---
+++ src/AssemblyInfo1.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("System.Numerics.DoubleVectors")]
+[assembly: AssemblyDescription("Copy of System.Numerics.Vectors except using double types")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Weingartner Machinenbau")]
+[assembly: AssemblyProduct("System.Numerics")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("77c07a1a-1451-4ac6-9f17-fe701d41ac05")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
